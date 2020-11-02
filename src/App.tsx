b575edb3c5d9e2c2d7b36c0a24f54ebdab28bed3
richardblondet <richardblondet@gmail.com>
2020-11-02 15:15:59 -0400

    Cleaning App root component

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,28 @@
-import React, { Suspense, lazy } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import errorBoundaryHOC, { Props as ErrorBoundaryProps } from './utils/errorBoundaryHOC';
-import { RootStoreProvider } from './store/providers';
-import { Reducer } from './store/reducer';
-
+import React from 'react';
+import { ErrorBoundary } from './components/Errors';
+import Routes from './routes';
 import './App.css';
 
-const Home = lazy(() => import('./routes/home'));
-
 /**
- * ErrorBundary 
- * @status rc
+ * Application
+ * 
+ * Towards version 1.0.0
+ * 
+ * @todo 
+  - Routing
+  - Internationalization
+  - Storybook
+  - Theming
+  - 
  */
-const ErrorBoundary = errorBoundaryHOC((props: ErrorBoundaryProps) => {
-  if (props.error) {
-   return (
-    <div className="error-screen">
-      <h2>An error has occured</h2>
-      <h4>{props.error.message}</h4>
-    </div>
-   );
-  }
-
-  return <React.Fragment>{props.children}</React.Fragment>;
-});
-
 const App = () => {
-  const state = {
-    foo: 'bar'
-  };
+  // return <div>hello world. starting with providers</div>;
 
   return (
     <ErrorBoundary>
-      <RootStoreProvider reducer={Reducer} initialState={state}>
-        <Router>
-          <Suspense fallback={<div>Loading...</div>}>
-            <Switch>
-              <Route exact path="/" component={Home}/>
-            </Switch>
-          </Suspense>
-        </Router>
-      </RootStoreProvider>
+      <Routes />
     </ErrorBoundary>
   );
 }
+
 export default App;
