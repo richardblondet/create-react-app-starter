9489e9fdf14be30e94456e0da3608d86514c5237
richardblondet <richardblondet@gmail.com>
2020-11-15 01:00:34 -0400

    new protected, auth and 403 routes for sess magnt

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
 import React, { lazy, Suspense } from 'react';
 import { Route, BrowserRouter, Switch } from 'react-router-dom';
 import { Loading } from '../components/Loaders';
+import { PrivateRoute } from '../store/utils';
 
 /**
  * Routes
@@ -22,15 +23,26 @@ import { Loading } from '../components/Loaders';
 
 const Home = lazy(() => import('./home'));
 const About = lazy(() => import('./about'));
+const Profile = lazy(() => import('./profile'));
+const Auth = lazy(() => import('./auth'));
 const Page404 = lazy(() => import('./404'));
+const Page403 = lazy(() => import('./403'));
 
 /** Routes Component */
 const Routes = () => (
   <BrowserRouter>
     <Suspense fallback={<Loading />}>
       <Switch>
+        {/* Public Routes */}
         <Route exact path="/" component={Home} />
         <Route path="/about" component={About} />
+        
+        {/* Private Routes */}
+        <PrivateRoute path="/me" component={Profile} isAuthenticated={true} />
+        
+        {/* "Utility" routes */}
+        <Route path="/auth" component={Auth} />
+        <Route path="/403" component={Page403} />
         <Route component={Page404} />
       </Switch>
     </Suspense>
