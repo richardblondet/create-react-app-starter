8e6f9e0470900d0e7ff1d2d6fec8c13777c311a9
richardblondet <richardblondet@gmail.com>
2020-11-15 00:57:24 -0400

    new auth component that will hold logic 4 authUsr

---
+++ src/routes/auth.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div className="App">
+      <h1>Login</h1>
+      <div>
+        <form>
+          <input type="text" />
+          <input type="password" />
+          <input type="submit" />
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
