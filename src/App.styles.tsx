d1b181fc186008963bbe629f1bd669d0205fd549
richardblondet <richardblondet@gmail.com>
2020-11-28 00:46:13 -0400

    reset styles

--- src/App.styles.tsx
+++ src/App.styles.tsx
@@ -1,41 +1,66 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle, css } from "styled-components";
 
-const GlobalStyle = createGlobalStyle`
-  body {
+/** Reset our styles */
+const reset = css`
+  /* http://meyerweb.com/eric/tools/css/reset/
+    v5.0.1 | 20191019
+    License: none (public domain)
+  */
+  html, body, div, span, applet, object, iframe,
+  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+  a, abbr, acronym, address, big, cite, code,
+  del, dfn, em, img, ins, kbd, q, s, samp,
+  small, strike, strong, sub, sup, tt, var,
+  b, u, i, center,
+  dl, dt, dd, menu, ol, ul, li,
+  fieldset, form, label, legend,
+  table, caption, tbody, tfoot, thead, tr, th, td,
+  article, aside, canvas, details, embed,
+  figure, figcaption, footer, header, hgroup,
+  main, menu, nav, output, ruby, section, summary,
+  time, mark, audio, video {
     margin: 0;
     padding: 0;
-    background: #f6f6f6;
+    border: 0;
+    font-size: 100%;
+    font: inherit;
+    vertical-align: baseline;
+  }
+  /* HTML5 display-role reset for older browsers */
+  article, aside, details, figcaption, figure,
+  footer, header, hgroup, main, menu, nav, section {
+    display: block;
+  }
+  /* HTML5 hidden-attribute fix for newer browsers */
+  *[hidden] {
+      display: none;
+  }
+  body {
+    line-height: 1;
+  }
+  menu, ol, ul {
+    list-style: none;
+  }
+  blockquote, q {
+    quotes: none;
+  }
+  blockquote:before, blockquote:after,
+  q:before, q:after {
+    content: '';
+    content: none;
+  }
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+  }
+`
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+
+  body {
+    
     font-family: 'Inter', Open-Sans, Helvetica, Arial, Sans-Serif;
   }
-  .App {
-    text-align: center;
-  }
-
-  .App-logo {
-    height: auto;
-    pointer-events: none;
-    max-width: 15em;
-  }
-
-  .App-header {
-    background-color: #282c34;
-    min-height: 100vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    font-size: calc(10px + 2vmin);
-    color: white;
-  }
-  .App-header.updated {
-    background-color: #FFF;
-    color: #333;
-  }
-  .App-link {
-    color: #61dafb;
-    font-size: 17px;
-  }
-
 `;
 
 export default GlobalStyle;
